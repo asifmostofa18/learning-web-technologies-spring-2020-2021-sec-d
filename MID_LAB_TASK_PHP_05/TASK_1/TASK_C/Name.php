f257e0242a1aa41281257acbc5822ef64a56a617
asifmostofa18 <asifmostofa18@gmail.com>
2021-02-18 20:15:24 +0600

    TASKC added

---
+++ MID_LAB_TASK_PHP_05/TASK_1/TASK_C/Name.php
@@ -0,0 +1,19 @@
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Name Input</title>
+</head>
+<body>
+	<form method="post" action="#">
+		<fieldset>
+			<legend>Name</legend>
+			<input type="text" name="myname" value="<?php if(isset($_POST['myname'])){ echo $_POST['myname'];} ?>"> <br>
+			
+			<input type="submit" name="submit" value="Submit">
+		</fieldset>
+	</form>
+	
+</body>
+</html>
