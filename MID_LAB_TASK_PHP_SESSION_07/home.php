c205cd02d943b4ef0f51b67ea45cdfbb8213a4c3
asifmostofa18 <asifmostofa18@gmail.com>
2021-02-25 23:47:40 +0600

    PUBLIC home is added

---
+++ MID_LAB_TASK_PHP_SESSION_07/home.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>PUBLIC HOME</title>
+</head>
+<body>
+    <fieldset>
+        <table width='100%' >
+            <tr>
+            <td>
+                    <img src='./images/logo.png' alt="home" height='50px'/>
+                </td> 
+                <td align='right'>
+                    <nav>
+                        <a href='./home.php'>Home</a> |
+                        <a href='./login.php'>Log In</a> |
+                        <a href='./registration.php'>Registration</a>
+                    </nav>
+                </td>
+            </tr>
+        </table>
+    </fieldset>
+    <fieldset>
+        <br> <br><br>
+        <label>
+            <h2>
+                Welcome to xCompany
+            </h2>
+        </label>
+        <br><br><br><br>
+    </fieldset>
+    <fieldset>
+        <center>
+            <label>
+                Copyright © 2017
+            </label>
+        </center>
+    </fieldset>
+</body>
+</html>
