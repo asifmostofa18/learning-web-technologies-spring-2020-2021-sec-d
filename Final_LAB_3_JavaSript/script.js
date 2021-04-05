6036b85609c92b3eac4709cbac4852d08ddd8c31
asifmostofa18 <asifmostofa18@gmail.com>
2021-04-05 22:37:48 +0600

    all item is okay now

--- Final_LAB_3_JavaSript/script.js
+++ Final_LAB_3_JavaSript/script.js
@@ -1,16 +1,3 @@
-"use strict"
-
-
-//alert('test');
-//console.log('testing...');
-
-//document.write('this is JS example');
-
-//document.getElementById('head').innerHTML = 'this is js example';
-//obj.innerHTML = "test";
-
-
-//let student = ['alamin', '123', 'CS'];
 
 function NameValidation()
 {
